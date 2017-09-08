b2c58723b4e6697079fdc5fee82b8febf907df46
Adam Jirasek <jiraseka@shrike.rnet>
2017-09-08 11:30:17 -0600

    Fix depeded section of dist/Makefile

---
+++ source/cfl3d/dist/depend_cmplx.mk
@@ -0,0 +1,121 @@
+# This file is generated automatically by fort_depend.py. DO NOT EDIT!
+
+findmin_new.o : 
+
+plot3d_2d.o : 
+
+qout.o :  \
+	../../cflcmplx/libs/module_kwstm.o \
+	../../cflcmplx/libs/module_contour.o \
+	../../cflcmplx/libs/module_profileout.o
+
+setslave.o : 
+
+updatedg.o : 
+
+qoutavg.o : 
+
+trnsfr_vals.o : 
+
+reass.o : 
+
+patcher.o : 
+
+plot3c.o : 
+
+forceout.o : 
+
+plot3d_coarse.o : 
+
+writ_buf.o : 
+
+usrint.o : 
+
+newalpha.o : 
+
+development.o : 
+
+bc_patch.o : 
+
+umalloc_r.o : 
+
+termn8.o : 
+
+wrestg.o : 
+
+pre_bc.o : 
+
+ae_corr.o :  \
+	../../cflcmplx/libs/genforce.o
+
+umalloc.o : 
+
+bc_blkint.o : 
+
+cputim.o : 
+
+calyplus.o : 
+
+resp.o : 
+
+qout_coarse.o : 
+
+sizer.o : 
+
+bc_period.o : 
+
+cfl3d.o :  \
+	mgbl.o
+
+plot3t.o : 
+
+wrest.o : 
+
+mgbl.o :  \
+	../../cflcmplx/libs/module_kwstm.o \
+	mgblk.o \
+	addnoneqstr.o
+
+compg2n.o : 
+
+rrest.o : 
+
+qinter.o : 
+
+updateg.o : 
+
+addnoneqstr.o : 
+
+bc_embed.o : 
+
+setup.o :  \
+	../../cflcmplx/libs/module_kwstm.o
+
+resetg.o : 
+
+prntcp.o : 
+
+plot3davg.o : 
+
+umalloc_c.o : 
+
+yplusout.o : 
+
+mgblk.o :  \
+	../../cflcmplx/libs/rb_corr.o \
+	ae_corr.o \
+	../../cflcmplx/libs/collq.o \
+	../../cflcmplx/libs/shift.o \
+	../../cflcmplx/libs/resid.o \
+	../../cflcmplx/libs/update.o
+
+plot3d.o : 
+
+qout_2d.o : 
+
+dynptch.o : 
+
+rrestg.o : 
+
+pointers.o :  \
+	../../cflcmplx/libs/module_kwstm.o
